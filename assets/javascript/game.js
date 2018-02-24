b3ec49d3d8384660f4c883aac92a067bea8a16b8
Unknown <jme919@gmail.com>
2018-02-23 22:23:05 -0500

    create random number generators for gem buttons and user#

---
+++ assets/javascript/game.js
@@ -0,0 +1,50 @@
+
+$(function() {
+    console.log( "its alive" );
+//*create a random number for user to match*//
+     
+    	var $randomNum = 19 + Math.floor(Math.random() * (120-19));
+    	$("#randomNum").text($randomNum);
+
+
+
+//*random hidden crystal values*//
+
+		var $blueNum = $('#blue').val(Math.floor(Math.random() * 12) + 1);
+
+		var $greenNum = $('#green').val(Math.floor(Math.random() * 12) + 1);
+
+		var $redNum = $('#red').val(Math.floor(Math.random() * 12) + 1);
+
+		var $yellowNum = $('#yellow').val(Math.floor(Math.random() * 12) + 1);
+
+    
+
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+	
